8cceae1588b641ffd9606fd7c1dd8ec306537e09
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-01-12 19:05:54 +0300

    Implement

---
+++ Fantasy-Quest/Assets/Scripts/Common/Result.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Common
+{
+    public abstract class Result
+    {
+        public bool Success { get; protected set; }
+        public bool Failure => !Success;
+
+        protected string Err;
+
+        public string Error =>
+            Failure
+                ? Err
+                : throw new Exception(
+                    $"You can't access .{nameof(Error)} when .{nameof(Failure)} is false"
+                );
+    }
+
+    public class SuccessResult : Result
+    {
+        public SuccessResult()
+        {
+            Success = true;
+        }
+    }
+
+    public class FailResult : Result
+    {
+        public FailResult(string error)
+        {
+            Success = false;
+            base.Err = error;
+        }
+    }
+}
---
+++ Fantasy-Quest/Assets/Scripts/Inventory/IInventory.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+using Item;
+
+namespace Inventory
+{
+    internal interface IInventory
+    {
+        public bool IsHasItem(string uid);
+        public Result AddItem(ItemImpl newItem);
+        public ValueResult<ItemImpl> TakeItem(ItemImpl item);
+    }
+}
