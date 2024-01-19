69eb6d503c2a698b51085ad0ac900f4b5059687e
ic3mintt <kgrints@gmail.com>
2024-01-19 16:33:55 +0300

    Add multiple subtitles occurence
    Now you can show multiple subtitles with customizable options in SubtitleDisplay. You shoud show subtitles with Subtitles. Should not use TextTween to show subtitles

---
+++ Fantasy-Quest/Assets/Scripts/SubtitlesSystem/ITextTween.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SubtitlesSystem
+{
+    public interface ITextTween
+    {
+        public event Action OnTextHiding;
+        public void Show(string text);
+        public void Hide();
+    }
+}
---
+++ Fantasy-Quest/Assets/Scripts/SubtitlesSystem/SubtitlesDisplay.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SubtitlesSystem
+{
+    [RequireComponent(typeof(TextTween))]
+    [AddComponentMenu("Scripts/SubtitlesSystem/SubtitlesSystem.SubtitlesDisplay")]
+    public class SubtitlesDisplay : MonoBehaviour
+    {
+        private bool isShowing;
+        private ITextTween iTextDisplay;
+        private Queue<string> subtitlesToShow;
+
+        private void Awake()
+        {
+            iTextDisplay = GetComponent<ITextTween>();
+            subtitlesToShow = new Queue<string>();
+        }
+
+        private void OnEnable()
+        {
+            iTextDisplay.OnTextHiding += ShowNext;
+        }
+
+        private void OnDisable()
+        {
+            iTextDisplay.OnTextHiding -= ShowNext;
+        }
+
+        public void Show(List<string> dialogue)
+        {
+            foreach (var text in dialogue)
+            {
+                Show(text);
+            }
+        }
+
+        private void Show(string text)
+        {
+            if (isShowing)
+            {
+                subtitlesToShow.Enqueue(text);
+            }
+            else
+            {
+                isShowing = true;
+                iTextDisplay.Show(text);
+            }
+        }
+
+        public void Hide()
+        {
+            subtitlesToShow.Clear();
+            iTextDisplay.Hide();
+        }
+
+        private void ShowNext()
+        {
+            if (subtitlesToShow.Any())
+            {
+                isShowing = true;
+                iTextDisplay.Show(subtitlesToShow.Dequeue());
+            }
+            else
+            {
+                isShowing = false;
+                iTextDisplay.Hide();
+            }
+        }
+    }
+}
