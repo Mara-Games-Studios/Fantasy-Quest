62fc5fa02dfe4d34bee40f099e3ed7b3feb08ccd
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-02-27 22:36:52 +0300

    some fixes

---
+++ Fantasy-Quest/Assets/Scripts/Dialogue/ChainSpeaker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using Sirenix.OdinInspector;
+using Subtitles;
+using TNRD;
+using UnityEngine;
+
+namespace Dialogue
+{
+    [AddComponentMenu("Scripts/Dialogue/Dialogue.ChainSpeaker")]
+    internal class ChainSpeaker : MonoBehaviour
+    {
+        [SerializeField]
+        private Replica replica;
+
+        [Required]
+        [SerializeField]
+        private AudioSource audioSource;
+
+        [SerializeField]
+        private SerializableInterface<ISubtitlesView> subtitles;
+
+        private Voice voice;
+
+        private void Awake()
+        {
+            voice = new(audioSource);
+        }
+
+        public void Tell(Action nextAction)
+        {
+            _ = StartCoroutine(TellRoutine(nextAction));
+        }
+
+        private IEnumerator TellRoutine(Action nextAction)
+        {
+            voice.Say(replica.Audio);
+            subtitles.Value.Show(replica);
+            yield return new WaitForSeconds(replica.Duration + replica.DelayAfterSaid);
+        }
+    }
+}
