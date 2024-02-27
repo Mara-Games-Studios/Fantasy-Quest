9b46c0641c0b0510bb174f31ecbd792e490d5588
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-02-27 16:09:54 +0300

    Implementation

---
+++ Fantasy-Quest/Assets/Scripts/Minigames/MouseInHay/Hay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Minigames.MouseInHay
+{
+    [AddComponentMenu("Scripts/Minigames/MouseInHay/Minigames.MouseInHay.Hay")]
+    internal class Hay : MonoBehaviour
+    {
+        [ReadOnly]
+        [SerializeField]
+        private List<Hole> holes;
+
+        [SerializeField]
+        private float showMouseTime;
+
+        [SerializeField]
+        private float noMouseTime;
+
+        private float WaitTime => showMouseTime + noMouseTime;
+
+        private void Awake()
+        {
+            holes = GetComponentsInChildren<Hole>().ToList();
+        }
+
+        private void Start()
+        {
+            _ = StartCoroutine(CallMouseAndWait(showMouseTime, WaitTime));
+        }
+
+        private IEnumerator CallMouseAndWait(float showMouseTime, float waitTime)
+        {
+            Hole hole = holes[Random.Range(0, holes.Count)];
+            hole.ShowMouse(showMouseTime);
+            yield return new WaitForSeconds(noMouseTime);
+            _ = StartCoroutine(CallMouseAndWait(showMouseTime, waitTime));
+        }
+    }
+}
