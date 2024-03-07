013f05541f586fabd93ded77914e106c1ddc82d7
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-03-08 00:22:42 +0300

    Introduce custom timeline track

---
+++ Fantasy-Quest/Assets/Scripts/TimelineTrack/TimeMachine/Clip.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using TNRD;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace TimelineTrack.TimeMachine
+{
+    [Serializable]
+    public class Clip : PlayableAsset, ITimelineClipAsset
+    {
+        private Behaviour template = new();
+
+        [Title("Marker Label")]
+        [SerializeField]
+        private string markerLabel = "";
+        public string MarkerLabel => markerLabel;
+
+        [Title("Action Type")]
+        [SerializeField]
+        private Action action;
+        public Action Action => action;
+
+        [SerializeField]
+        [ShowIf(nameof(action), Action.JumpToMarker)]
+        private string markerToJumpTo = "";
+        public string MarkerToJumpTo => markerToJumpTo;
+
+        [SerializeField]
+        [ShowIf(nameof(action), Action.JumpToTime)]
+        private float timeToJumpTo = 0f;
+        public float TimeToJumpTo => timeToJumpTo;
+
+        [Title("Condition")]
+        [SerializeField]
+        private ConditionType condition;
+        public ConditionType ConditionType => condition;
+
+        [SerializeField]
+        [ShowIf(nameof(condition), ConditionType.Custom)]
+        private List<SerializableInterface<IConditionSource>> referenceCondition;
+        public IEnumerable<IConditionSource> Conditions => referenceCondition.Select(x => x.Value);
+
+        public ClipCaps clipCaps => ClipCaps.None;
+
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            ScriptPlayable<Behaviour> playable = ScriptPlayable<Behaviour>.Create(graph, template);
+            Behaviour behaviour = playable.GetBehaviour();
+            behaviour.AssignValuesFromClip(this);
+            return playable;
+        }
+    }
+}
---
+++ Fantasy-Quest/Assets/Scripts/TimelineTrack/TimeMachine/Track.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace TimelineTrack.TimeMachine
+{
+    [TrackColor(0.74f, 0.33f, 0.85f)]
+    [TrackClipType(typeof(Clip))]
+    public class Track : TrackAsset
+    {
+        public override Playable CreateTrackMixer(
+            PlayableGraph graph,
+            GameObject go,
+            int inputCount
+        )
+        {
+            ScriptPlayable<MixerBehaviour> scriptPlayable = ScriptPlayable<MixerBehaviour>.Create(
+                graph,
+                inputCount
+            );
+            MixerBehaviour mixerBehaviour = scriptPlayable.GetBehaviour();
+            mixerBehaviour.MarkerClips = new Dictionary<string, double>();
+
+            foreach (TimelineClip timeLineClip in GetClips())
+            {
+                Clip timeMachineClip = timeLineClip.asset as Clip;
+
+                timeLineClip.displayName = timeMachineClip.Action switch
+                {
+                    Action.Marker => "● " + timeMachineClip.MarkerLabel.ToString(),
+                    Action.JumpToTime => "↩ " + timeMachineClip.TimeToJumpTo.ToString(),
+                    Action.JumpToMarker => "↩︎  " + timeMachineClip.MarkerToJumpTo.ToString(),
+                    Action.Pause => "||",
+                    _ => throw new System.ArgumentException(),
+                };
+
+                if (timeMachineClip.Action == Action.Marker)
+                {
+                    if (!mixerBehaviour.MarkerClips.ContainsKey(timeMachineClip.MarkerLabel))
+                    {
+                        mixerBehaviour.MarkerClips.Add(
+                            timeMachineClip.MarkerLabel,
+                            (double)timeLineClip.start
+                        );
+                    }
+                }
+            }
+
+            return scriptPlayable;
+        }
+    }
+}
