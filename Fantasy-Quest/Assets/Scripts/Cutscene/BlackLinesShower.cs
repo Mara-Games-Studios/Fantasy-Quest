7a7ebd20bfbdb95494eeb02ce2aca6e9f93e68af
ic3mintt <kgrints@gmail.com>
2024-03-08 00:21:57 +0300

    Add namespace

--- Fantasy-Quest/Assets/Scripts/Cutscene/BlackLinesShower.cs
+++ Fantasy-Quest/Assets/Scripts/Cutscene/BlackLinesShower.cs
@@ -4,48 +4,51 @@ using DG.Tweening;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-[AddComponentMenu("Scripts/Cutscene/Cutscene.BlackLinesShower")]
-public class BlackLinesShower : MonoBehaviour
+namespace Cutscene
 {
-    [Serializable]
-    private struct Line
+    [AddComponentMenu("Scripts/Cutscene/Cutscene.BlackLinesShower")]
+    public class BlackLinesShower : MonoBehaviour
     {
-        public RectTransform StartPoint;
-        public RectTransform EndPoint;
-        public UnityEngine.UI.Image Image;
-    }
-
-    [SerializeField]
-    private float duration;
-
-    [SerializeField]
-    [Range(0f, 1f)]
-    private float targetAlpha;
-
-    [SerializeField]
-    private List<Line> lines;
-
-    // Prepared to be invoked by Timeline.
-    [Button]
-    public void Show()
-    {
-        foreach (Line line in lines)
+        [Serializable]
+        private struct Line
         {
-            line.Image.rectTransform.position = line.StartPoint.position;
-            _ = line.Image.rectTransform.DOMove(line.EndPoint.position, duration);
-            _ = line.Image.DOFade(targetAlpha, duration);
+            public RectTransform StartPoint;
+            public RectTransform EndPoint;
+            public UnityEngine.UI.Image Image;
         }
-    }
 
-    // Prepared to be invoked by Timeline.
-    [Button]
-    public void Hide()
-    {
-        foreach (Line line in lines)
+        [SerializeField]
+        private float duration;
+
+        [SerializeField]
+        [Range(0f, 1f)]
+        private float targetAlpha;
+
+        [SerializeField]
+        private List<Line> lines;
+
+        // Prepared to be invoked by Timeline.
+        [Button]
+        public void Show()
         {
-            line.Image.rectTransform.position = line.EndPoint.position;
-            _ = line.Image.rectTransform.DOMove(line.StartPoint.position, duration);
-            _ = line.Image.DOFade(0f, duration);
+            foreach (Line line in lines)
+            {
+                line.Image.rectTransform.position = line.StartPoint.position;
+                _ = line.Image.rectTransform.DOMove(line.EndPoint.position, duration);
+                _ = line.Image.DOFade(targetAlpha, duration);
+            }
+        }
+
+        // Prepared to be invoked by Timeline.
+        [Button]
+        public void Hide()
+        {
+            foreach (Line line in lines)
+            {
+                line.Image.rectTransform.position = line.EndPoint.position;
+                _ = line.Image.rectTransform.DOMove(line.StartPoint.position, duration);
+                _ = line.Image.DOFade(0f, duration);
+            }
         }
     }
 }
