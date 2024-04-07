6c54350c2f8f019a443dbcf5941551d0db98885e
Mixremin <omega8428@gmail.com>
2024-04-07 13:30:59 +0300

    Review fixes

--- Fantasy-Quest/Assets/Scripts/DialogueBubble/EmoteBubbleRotate.cs
+++ Fantasy-Quest/Assets/Scripts/DialogueBubble/EmoteBubbleRotate.cs
@@ -7,6 +7,7 @@ namespace DialogueBubble
     [AddComponentMenu("Scripts/DialogueBubble/DialogueBubble.EmoteBubbleRotate")]
     internal class EmoteBubbleRotate : MonoBehaviour
     {
+        [Required]
         [SerializeField]
         private Movement catMovement;
 
