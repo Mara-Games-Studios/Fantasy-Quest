ed00ff349b3c72ad8b3f57321a07a65b62bc35ea
Mixremin <omega8428@gmail.com>
2024-04-08 17:48:34 +0300

    New barn initial

---
+++ Fantasy-Quest/Assets/Scripts/LevelSpecific/ForestEdge/ChickenBehaviour.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine; 
+
+namespace LevelSpecific.ForestEdge
+{
+    [AddComponentMenu("Scripts/LevelSpecific/ForestEdge/LevelSpecific.ForestEdge.ChickenBehaviour")] 
+    internal class ChickenBehaviour : MonoBehaviour 
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
---
+++ Fantasy-Quest/Assets/Scripts/LevelSpecific/ForestEdge/ChickenView.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine; 
+
+namespace LevelSpecific.ForestEdge
+{
+    [AddComponentMenu("Scripts/LevelSpecific/ForestEdge/LevelSpecific.ForestEdge.ChickenView")] 
+    internal class ChickenView : MonoBehaviour 
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
