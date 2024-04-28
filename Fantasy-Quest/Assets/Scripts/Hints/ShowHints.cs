052c2bbb68b445c42f3218bd08c80d7786eef523
Mixremin <omega8428@gmail.com>
2024-04-28 23:09:12 +0300

    Timer hints

--- Fantasy-Quest/Assets/Scripts/Hints/ShowHints.cs
+++ Fantasy-Quest/Assets/Scripts/Hints/ShowHints.cs
@@ -1,7 +1,9 @@
+using System.Collections;
 using System.Collections.Generic;
 using Configs;
 using DG.Tweening;
 using Interaction;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Hints
@@ -11,6 +13,13 @@ namespace Hints
     {
         [Header("Show Settings")]
         [SerializeField]
+        private bool alwaysShowWithoutTimer = false;
+
+        [ShowIf("@!alwaysShowWithoutTimer")]
+        [SerializeField]
+        private bool firstShowWithoutTimer = true;
+
+        [SerializeField]
         private float fadeInDuration = 0.6f;
 
         [SerializeField]
@@ -19,6 +28,7 @@ namespace Hints
         [SerializeField]
         private List<SpriteRenderer> keyboardSpriteRenderer;
 
+        private Coroutine waitRoutine;
         private Tween fadeTween;
 
         private void Awake()
@@ -33,11 +43,24 @@ namespace Hints
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (!LockerSettings.Instance.IsDialogueBubbleLocked)
+            if (
+                !LockerSettings.Instance.IsDialogueBubbleLocked
+                && (firstShowWithoutTimer || alwaysShowWithoutTimer)
+            )
             {
                 if (collision.TryGetComponent(out InteractionImpl _))
                 {
                     FadeIn(fadeInDuration);
+                    firstShowWithoutTimer = false;
+                }
+            }
+            else if (!LockerSettings.Instance.IsDialogueBubbleLocked && !firstShowWithoutTimer)
+            {
+                if (collision.TryGetComponent(out InteractionImpl _))
+                {
+                    waitRoutine = StartCoroutine(
+                        WaitUntilShowRoutine(HintConfig.Instance.SecondUntilShow)
+                    );
                 }
             }
         }
@@ -48,9 +71,20 @@ namespace Hints
             {
                 if (collision.TryGetComponent(out InteractionImpl _))
                 {
+                    if (waitRoutine != null)
+                    {
+                        StopCoroutine(waitRoutine);
+                    }
+
                     FadeOut(fadeOutDuration);
                 }
             }
+        }
+
+        private IEnumerator WaitUntilShowRoutine(float duration)
+        {
+            yield return new WaitForSeconds(duration);
+            FadeIn(fadeInDuration);
         }
 
         public void FadeIn(float duration)
