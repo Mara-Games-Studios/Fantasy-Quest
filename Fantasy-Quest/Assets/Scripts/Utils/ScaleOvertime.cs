b3a3ed898c76920dd37ebefa43928faf4ac13dd6
Mixremin <omega8428@gmail.com>
2024-05-09 19:26:15 +0300

    Jump on tree cutscenes

---
+++ Fantasy-Quest/Assets/Scripts/Utils/ScaleOvertime.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Utils
+{
+    [AddComponentMenu("Scripts/Utils/Utils.ScaleOvertime")]
+    internal class ScaleOvertime : MonoBehaviour
+    {
+
+        [SerializeField]
+        private bool fromToMode = false;
+
+        [SerializeField]
+        private Transform target;
+
+        [ShowIf(nameof(fromToMode))]
+        [SerializeField]
+        private Vector3 fromScale;
+
+        [SerializeField]
+        private Vector3 toScale;
+
+        [SerializeField]
+        private float duration;
+
+        private void ScaleSnapshot()
+        {
+            if (!fromToMode)
+            {
+                fromScale = target.localScale;
+            }
+        }
+
+        public void ChangeScale()
+        {
+            ScaleSnapshot();
+            _ = StartCoroutine(ScaleChangerRoutine());
+        }
+
+        private IEnumerator ScaleChangerRoutine()
+        {
+            float timer = 0;
+            while (timer < duration)
+            {
+                timer += Time.deltaTime;
+
+                target.localScale = Vector3.Lerp(fromScale, toScale, timer / duration);
+
+                yield return null;
+            }
+        }
+    }
+}
