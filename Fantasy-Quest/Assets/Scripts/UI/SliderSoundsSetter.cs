485cec0d162c29bd299515108a4685a8aeaf5c00
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-05-12 03:05:15 +0300

    Add more bug fixes

---
+++ Fantasy-Quest/Assets/Scripts/UI/SliderSoundsSetter.cs
@@ -0,0 +1,19 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [AddComponentMenu("Scripts/UI/UI.SliderSoundsSetter")]
+    internal class SliderSoundsSetter : MonoBehaviour
+    {
+        [Required]
+        [SerializeField]
+        private Slider slider;
+
+        private void Start()
+        {
+            slider.value = Configs.AudioSettings.Instance.SoundsValue;
+        }
+    }
+}
---
+++ Fantasy-Quest/Assets/Scripts/Utils/BackgroundSupport/ButterflyFlipper.cs
@@ -0,0 +1,40 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Utils.BackgroundSupport
+{
+    [AddComponentMenu("Scripts/Utils/BackgroundSupport/Utils.BackgroundSupport.ButterflyFlipper")]
+    internal class ButterflyFlipper : MonoBehaviour
+    {
+        [Required]
+        [SerializeField]
+        private Transform targetTransform;
+
+        [SerializeField]
+        private Vector3 leftRotation;
+
+        [SerializeField]
+        private Vector3 rightRotation;
+
+        private float previousX;
+
+        private void Start()
+        {
+            previousX = targetTransform.position.x;
+        }
+
+        [Button]
+        public void UpdateRotation()
+        {
+            if (previousX < targetTransform.position.x)
+            {
+                targetTransform.rotation = Quaternion.Euler(rightRotation);
+            }
+            else
+            {
+                targetTransform.rotation = Quaternion.Euler(leftRotation);
+            }
+            previousX = targetTransform.position.x;
+        }
+    }
+}
