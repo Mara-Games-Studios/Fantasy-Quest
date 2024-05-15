9a968edc9cc787844c1b40752fc1eb2ff362bb03
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-05-15 04:39:37 +0300

    Add dialogue speaker changing

---
+++ Fantasy-Quest/Assets/Scripts/Dialogue/UpdateTargetDialogueSpeaker.cs
@@ -0,0 +1,31 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Dialogue
+{
+    [AddComponentMenu("Scripts/Dialogue/Dialogue.UpdateTargetDialogueSpeaker")]
+    internal class UpdateTargetDialogueSpeaker : MonoBehaviour
+    {
+        [Required]
+        [SerializeField]
+        private DialogueSpeaker dialogueSpeaker;
+
+        [SerializeField]
+        private int firstTrySpeechIndex = 0;
+
+        [SerializeField]
+        private int alternativeSpeechIndex = 0;
+
+        [Button]
+        public void UpdateFirstTrySpeechText(string text)
+        {
+            dialogueSpeaker.FFirstTrySpeech[firstTrySpeechIndex].UpdateString(text);
+        }
+
+        [Button]
+        public void UpdateAlternativeSpeechText(string text)
+        {
+            dialogueSpeaker.AAlternativeSpeech[alternativeSpeechIndex].UpdateString(text);
+        }
+    }
+}
