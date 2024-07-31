1f4003f15b9d284262f41cc768c15be4be51db5b
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-07-31 15:21:32 +0300

    Fixes by review + small asset rearranging

--- Fantasy-Quest/Assets/Editor/Common/AssetToolsCommands.cs
+++ Fantasy-Quest/Assets/Editor/Common/AssetToolsCommands.cs
@@ -1,19 +1,15 @@
-﻿#if UNITY_EDITOR
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-#endif
 
 namespace Common
 {
     internal static class AssetsToolsCommands
     {
-#if UNITY_EDITOR
-        [MenuItem("Asset Tools/Force Reserialize ALL Assets")]
+        [MenuItem("Tools/Force Reserialize ALL Assets")]
         public static void ForceReserializeAssets()
         {
             string[] guids = AssetDatabase.FindAssets("", null);
-            Debug.Log("Start Reserialize " + guids.Length + " assets.");
             foreach (string guid in guids)
             {
                 Reserialize(guid);
@@ -35,6 +31,5 @@ namespace Common
                 Debug.Log("Error in " + path);
             }
         }
-#endif
     }
 }
