fbb5fc3aad5287e3430983c0d529a3712b1ed895
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-08-02 21:51:24 +0300

    Create project lifetime

---
+++ Fantasy-Quest/Assets/Scripts/DI/ProjectLifetime.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace DI
+{
+    [AddComponentMenu("Scripts/DI/DI.ProjectLifetime")]
+    internal class ProjectLifetime : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder) { }
+    }
+}
