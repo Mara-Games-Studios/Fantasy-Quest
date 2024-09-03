d2a4a0f11008ed98d7870a0b5e9be57fe17e1849
Adammui <adami.yuje@gmail.com>
2024-09-04 00:07:31 +0300

    Added pushing knot in both directions and changed old spine model.

--- Fantasy-Quest/Assets/Scripts/LevelSpecific/House/Knot.cs
+++ Fantasy-Quest/Assets/Scripts/LevelSpecific/House/Knot.cs
@@ -1,6 +1,13 @@
+using System;
+using System.Collections.Generic;
+using Cat;
+using Configs;
+using Cysharp.Threading.Tasks;
 using Interaction;
+using Spine.Unity;
 using UnityEngine;
 using UnityEngine.Events;
+using VContainer;
 
 namespace LevelSpecific.House
 {
@@ -8,57 +15,54 @@ namespace LevelSpecific.House
     [AddComponentMenu("Scripts/LevelSpecific/House/LevelSpecific.House.Knot")]
     internal class Knot : MonoBehaviour, IInteractable
     {
-        private enum State
-        {
-            Left,
-            Right
-        }
+        [Inject]
+        private LockerApi lockerSettings;
 
         [SerializeField]
-        private bool canCatInteract = true;
+        private Movement catMovement;
 
         [SerializeField]
         private Animator animator;
 
         [SerializeField]
-        private State state = State.Left;
+        private SkeletonAnimation skeletonAnimation;
+
+        private static List<int> positionsList = new() { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        private LinkedList<int> knotPosition = new(positionsList);
 
         public UnityEvent OnKnotHinted;
 
         public void Interact()
         {
-            if (canCatInteract)
+            if (!lockerSettings.Api.IsCatInteractionLocked)
             {
+                lockerSettings.Api.LockForCarryingItem();
                 OnKnotHinted?.Invoke();
-                switch (state)
+                Cat.Vector vector = catMovement.Vector;
+                if (vector == Cat.Vector.Right && knotPosition.Last.Value != 1)
                 {
-                    case State.Left:
-                        canCatInteract = false;
-                        animator.SetTrigger("MoveToRight");
-                        state = State.Right;
-                        break;
-                    case State.Right:
-                        canCatInteract = false;
-                        animator.SetTrigger("MoveToLeft");
-                        state = State.Left;
-                        break;
+                    knotPosition.RemoveLast();
+                    _ = knotPosition.AddFirst(0);
+                    skeletonAnimation.AnimationName = "MoveRight";
+                    animator.SetTrigger("MoveToRight");
                 }
+                else if (vector == Cat.Vector.Left && knotPosition.First.Value != 1)
+                {
+                    knotPosition.RemoveFirst();
+                    _ = knotPosition.AddLast(0);
+                    skeletonAnimation.AnimationName = "MoveLeft";
+                    animator.SetTrigger("MoveToLeft");
+                }
+                _ = UnlockInteraction();
             }
         }
 
-        public void InteractByHuman()
+        private async UniTask UnlockInteraction()
         {
+            await UniTask.Delay(TimeSpan.FromSeconds(1));
+            lockerSettings.Api.UnlockAll();
             return;
-        }
-
-        public void SetTrueCanInteract()
-        {
-            canCatInteract = true;
-        }
-
-        public void SetFalseCanInteract()
-        {
-            canCatInteract = false;
         }
     }
 }
