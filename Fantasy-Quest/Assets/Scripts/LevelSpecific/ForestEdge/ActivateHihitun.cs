72b341675bafc69990365d5e65cffa49371fe6e5
Adammui <adami.yuje@gmail.com>
2024-09-12 12:15:30 +0300

    Fix Hihitun laugh.

--- Fantasy-Quest/Assets/Scripts/LevelSpecific/ForestEdge/ActivateHihitun.cs
+++ Fantasy-Quest/Assets/Scripts/LevelSpecific/ForestEdge/ActivateHihitun.cs
@@ -1,5 +1,7 @@
+using Audio;
 using Effects;
 using Interaction;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace LevelSpecific.ForestEdge
@@ -8,11 +10,17 @@ namespace LevelSpecific.ForestEdge
     [AddComponentMenu("Scripts/LevelSpecific/ForestEdge/LevelSpecific.ForestEdge.ActivateHihitun")]
     internal class ActivateHihitun : MonoBehaviour
     {
+        [Required]
         [SerializeField]
         private GameObject objectToShow;
 
+        [Required]
         [SerializeField]
         private Fade hihitunLastPlace;
+
+        [Required]
+        [SerializeField]
+        private SoundPlayer hihitunLaugh;
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
@@ -20,6 +28,7 @@ namespace LevelSpecific.ForestEdge
             {
                 gameObject.SetActive(false);
                 hihitunLastPlace.Disappear();
+                hihitunLaugh.PlayClip();
                 if (interaction.JumpTrigger.IsJumping)
                 {
                     interaction.JumpTrigger.AddOneTimeEndJumpCallback(
